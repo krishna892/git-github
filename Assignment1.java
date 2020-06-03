ec81d3ec13385a787604c0b91a369d57ea0e4aa0
krishna892 <66386532+krishna892@users.noreply.github.com>
2020-06-04 01:21:32 +0530

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,30 @@
+package com.edureka.module2;
+
+class Multiplication {
+
+	static int multiply(int a, int b)
+	{
+		return a*b;
+	}  
+}
+
+class Multiplication_float extends Multiplication {
+
+	static float multiply(float a, float b)
+	{
+		return a*b;
+	}  
+
+}
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Multiplication_float obj = new Multiplication_float();
+		System.out.println(obj.multiply(12.7f, 23.45f));
+		System.out.println(obj.multiply(1234, 2345));
+	}
+
+}
